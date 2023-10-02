497f1d900816d9e691acb339c01467931e21141b
nervu <karikarij11@gmail.com>
2023-10-02 09:41:40 -0600

    wip

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function() {
+    document.querySelector('.top-nav').classList.toggle('show');
+    let expanded = this.getAttribute('aria-expanded') == 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+    // these 2 are nice important lines?
+});
